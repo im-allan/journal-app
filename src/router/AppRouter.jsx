caf164c900137b690a5d44e8c06293f61790e705
Allan <al.silvaquiroz@gmail.com>
2023-04-25 16:01:37 -0400

    feat(router): routes structure

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        {/* Login */}
+        <Route />
+        {/* Journal App */}
+        <Route />
+    </Routes>
+
+  )
+}
