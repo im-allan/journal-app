4dd2aea283a14a390e2a18db6d85afc62a04fbbe
Allan <al.silvaquiroz@gmail.com>
2023-04-26 22:08:54 -0400

    feat(journal): added journal page w/ navbar & sidebar components

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,15 @@
+import { Typography, Grid } from '@mui/material';
+import { JournalLayout } from "../layout/JournalLayout";
+
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+
+      <Typography>Lorem, ipsum.</Typography>
+
+      {/* Nothing Selected */}
+      {/* NoteView */}
+    </JournalLayout>
+  )
+}
