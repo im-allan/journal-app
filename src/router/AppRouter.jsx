3d7379d83044177e6cb2cfab4548a7f47f888af6
Allan <al.silvaquiroz@gmail.com>
2023-04-28 22:04:36 -0400

    feat(router): added journal routes to main app router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
 
 export const AppRouter = () => {
   return (
     <Routes>
         {/* Login */}
-        <Route />
+        <Route path="/auth/*" element={ <AuthRoutes /> } />
         {/* Journal App */}
-        <Route />
+        <Route path="/*" element={ <JournalRoutes /> } />
     </Routes>
-
   )
 }
